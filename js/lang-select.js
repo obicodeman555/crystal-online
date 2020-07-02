13a15abe712f965f5524d3cf285d7bde2c3c4914
obicodeman555 <obicodeman@yahoo.com>
2020-07-02 09:57:50 +0100

    first commit

---
+++ js/lang-select.js
@@ -0,0 +1,16 @@
+const selectItems = document.querySelectorAll('.lang-select');
+const langSelect = () => {
+
+    selectItems.forEach(selectItem => {
+    const inputField = selectItem.querySelector('.input-field');
+    const icon = selectItem.querySelector('.icon-right');
+    const ul = selectItem.querySelector('.select-item');
+
+    inputField.addEventListener('click', (event) => {
+      event.preventDefault()
+      ul.classList.toggle('active');
+      icon.classList.toggle('active');
+    })
+  })
+}
+langSelect();
