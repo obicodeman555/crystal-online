83d582a83a339f73cb9a43e65b70b22d5fa17c46
obicodeman555 <obicodeman@yahoo.com>
2020-07-06 19:32:10 +0100

    create slider for review section

---
+++ js/slide.js
@@ -0,0 +1,9 @@
+new Glider(document.querySelector('.glider'), {
+    slidesToScroll: 1,
+    slidesToShow: 3.5,
+    draggable: true,
+    arrows: {
+      prev: '.glider-prev',
+      next: '.glider-next'
+    }
+  });
